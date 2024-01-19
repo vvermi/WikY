5f53e4f67e2a7414f7d4f22b8d04027deb55bb88
vvermi <v.vermi@gmail.com>
2024-01-19 10:22:37 +0100

    Ajout Test Repository

---
+++ RepositoriesTests/ArticleRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Tests
+{
+    [TestClass()]
+    public class ArticleRepositoryTests
+    {
+
+        [TestMethod()]
+        public async Task ReadTest()
+        {
+
+            var builder =new  DbContextOptionsBuilder<Context>().UseInMemoryDatabase("articles");
+
+            var context = new Context(builder.Options);
+            context.Database.EnsureDeleted();
+            context.Articles.Add(new Entities.Article() { Id = 1, Auteur = "moi", Contenu="c1", Theme="t1" });
+            context.Articles.Add(new Entities.Article() { Id = 2, Auteur = "toi" , Contenu="c2", Theme="t2"});
+
+            context.SaveChanges();
+
+            ArticleRepository articleRepository = new ArticleRepository(context);
+
+            var article = await articleRepository.Read(2);
+
+            Assert.AreEqual(article.Auteur, "toi");
+        }
+    }
+}
